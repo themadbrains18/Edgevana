37f1ba1be3cbddf27a98cd6657ab7610c12b50e2
PankajTMB <pankaj@themadbrains.com>
2022-01-01 12:38:10 +0530

    add index scripts

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,7 +2,6 @@
 let toggleBtn = document.querySelector(".toggle__btn svg");
 let navlist = document.querySelector(".nav__list");
 
-
 toggleBtn.addEventListener("click",()=>{
     toggleBtn.classList.toggle("cross");
     navlist.classList.toggle("show");
